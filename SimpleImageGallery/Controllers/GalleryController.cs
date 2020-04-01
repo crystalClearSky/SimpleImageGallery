3c9031bb692e5e87b0d536c8fa5d7c23c62e78cc
crystalClearSky <jheadbush@gmail.com>
2020-04-01 14:28:14 +0100

    Add project files.

---
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data;
+using SimpleImageGallery.Data.Models;
+using SimpleImageGallery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly IImage _imageService;
+        public GalleryController(IImage imageService)
+        {
+            _imageService = imageService;
+        }
+        public IActionResult Index()
+        {
+            var imageList = _imageService.GetAll();
+            var model = new GalleryIndexModel()
+            {
+                Images = imageList,
+                SearchQuery = ""
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SimpleImageGallery/Models/GalleryIndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SimpleImageGallery.Data.Models;
+
+namespace SimpleImageGallery.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> Images { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
