3489b674de4a3ea2e82a15bb03064d8913907e5a
crystalClearSky <jheadbush@gmail.com>
2020-04-02 17:56:09 +0100

    Finally complete tut!

--- SimpleImageGallery.Data/IImage.cs
+++ SimpleImageGallery.Data/IImage.cs
@@ -1,5 +1,8 @@
-﻿using SimpleImageGallery.Data.Models;
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using SimpleImageGallery.Data.Models;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SimpleImageGallery.Data
 {
@@ -8,5 +11,9 @@ namespace SimpleImageGallery.Data
         IEnumerable<GalleryImage> GetAll();
         IEnumerable<GalleryImage> GetWithTag(string tag);
         GalleryImage GetById(int id);
+        CloudBlobContainer GetBlobContainer(string connectionString, string containerName);
+        Task SetImage(string title, string tags, Uri uri);
+        List<ImageTag> ParseTags(string tags);
+    
     }
 }
